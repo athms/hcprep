8bcca489d59e3c9a325ddfdd91682764a5954413
athm <armin.thms@gmail.com>
2020-01-20 15:49:03 +0100

    example script for data download

---
+++ example_download.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+import os
+import argparse
+import numpy as np
+import tfMRI_HCP_downloader
+
+
+def make_sure_path_exists(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory, exist_ok=True)
+
+
+if __name__ == '__main__':
+
+	# retrieve ACCESS KEY & SECRET KEY
+	ap = argparse.ArgumentParser()
+	# add arguments to parser
+	ap.add_argument("--ACCESS_KEY", required=True,
+	   help="AWS S3 access key")
+	ap.add_argument("--SECRET_KEY", required=True,
+	   help="AWS S3 access key")
+	ap.add_argument("--path", required=False,
+	   help="AWS S3 access key")
+	ap.add_argument("--n_subjects", required=False
+,	   help="AWS S3 access key")
+	args = vars(ap.parse_args())
+	# set variables
+	ACCESS_KEY = str(args['ACCESS_KEY'])
+	SECRET_KEY = str(args['SECRET_KEY'])
+	if args['path'] is not None:
+		output_path = str(args['path'])
+		print('Path: {}'.format(output_path))
+	else:
+		output_path = 'data/'
+		print('"path" not defined. Defaulting to: {}'.format(output_path))
+	make_sure_path_exists(output_path)
+	if args['n_subjects'] is not None:
+		n_subjects = int(args['n_subjects'])
+	else:
+		n_subjects = 2
+	print('n_subjects to download: {}'.format(n_subjects))
+
+	# HCP exp descriptors
+	tasks = ['EMOTION',
+			 'GAMBLING',
+			 'LANGUAGE',
+			 'MOTOR',
+			 'RELATIONAL',
+			 'SOCIAL',
+			 'WM']
+	runs = ['LR', 'RL']
+
+	# download subject data 
+	for task in tasks:
+		task_subjects = np.load('subject_ids/tfMRI_{}_subject_ids.npy'.format(task))[:n_subjects]
+		for subject in task_subjects:
+			for run in runs:
+				tfMRI_HCP_downloader.download.download_hcp_subject_data(ACCESS_KEY, SECRET_KEY, subject, task, run, output_path)
