6eae2997117734692697b896d41d2f14c4e9061c
rminthms <armin.thms@gmail.com>
2020-01-21 14:08:43 +0100

    adding example script to deomonstrate prepricessing and writing to tfr data format

---
+++ example_preprocess.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+import os
+import argparse
+import numpy as np
+import tfMRI_HCP_downloader
+
+
+if __name__ == "__main__":
+
+	# retrieve ACCESS KEY & SECRET KEY
+    ap = argparse.ArgumentParser()
+    # add arguments to parser
+    ap.add_argument("--path", required=False,
+                    help="AWS S3 access key")
+    ap.add_argument("--n_tfr_writers", required=False,
+                    help="AWS S3 access key")
+    args = vars(ap.parse_args())
+    # set variables
+    if args['path'] is not None:
+        path = str(args['path'])
+        print('Path: {}'.format(path))
+    else:
+        path = 'data/'
+        print('"path" not defined. Defaulting to: {}'.format(path))
+
+    # extract subject IDs
+    subjects = [int(f.split('sub-')[1]) for f in os.listdir(path) if 'sub' in f]
+    n_tfr_writers = 1
+
+    # HCP exp descriptors
+    TR = 0.72
+    tasks = ['EMOTION',
+             'GAMBLING',
+             'LANGUAGE',
+             'MOTOR',
+             'RELATIONAL',
+             'SOCIAL',
+             'WM']
+    runs = ['LR', 'RL']
+    n_classes_per_task = [2, 3, 2, 5, 2, 2, 4]
+
+    # tfr path
+    tfr_path = path+'tfr/'
+    tfMRI_HCP_downloader.paths.make_sure_path_exists(tfr_path)
+    
+    # write data
+    for subject_id, subject in enumerate(subjects):
+    	for task_id, task in enumerate(tasks):
+    		for run_id, run in enumerate(runs):
+    			tfr_writers = [tf.python_io.TFRecordWriter(
+    				tfr_path+'task-{}_subject-{}_{}.tfrecords'.format(task, subject, wi)) for wi in range(n_tfr_writers)]
+	    		subject_data = tfMRI_HCP_downloader.data.load_subject_data(task, subject, run, path, TR)
+	    		volumes, volume_labels = tfMRI_HCP_downloader.preprocess.preprocess_subject_data(
+	    			subject_data, [run], high_pass=1./128., smoothing_fwhm=3)
+	    		tfMRI_HCP_downloader.convert.write_to_tfr(tfr_writers,
+										                  volumes, volume_labels,
+										                  subject_id, task_id, run_id, n_classes_per_task,
+										                  randomize_volumes=Tru)
+	    
