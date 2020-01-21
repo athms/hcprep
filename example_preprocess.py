36a9b0028a3c8b176424198255be7397f495d341
athm <armin.thms@gmail.com>
2020-01-21 16:27:21 +0100

    expanding example for data preprocessing

--- example_preprocess.py
+++ example_preprocess.py
@@ -2,17 +2,18 @@
 import os
 import argparse
 import numpy as np
+import tensorflow as tf
 import tfMRI_HCP_downloader
 
 
 if __name__ == "__main__":
 
-	# retrieve ACCESS KEY & SECRET KEY
+    # retrieve ACCESS KEY & SECRET KEY
     ap = argparse.ArgumentParser()
     # add arguments to parser
     ap.add_argument("--path", required=False,
                     help="AWS S3 access key")
-    ap.add_argument("--n_tfr_writers", required=False,
+    ap.add_argument("--n_tfr", required=False,
                     help="AWS S3 access key")
     args = vars(ap.parse_args())
     # set variables
@@ -22,13 +23,17 @@ if __name__ == "__main__":
     else:
         path = 'data/'
         print('"path" not defined. Defaulting to: {}'.format(path))
+    if args['n_tfr'] is not None:
+        n_tfr_writers = int(args['n_tfr'])
+    else:
+        n_tfr_writers = 1
+    print('Number of TFR-writers per [task, subject, run]: {}'.format(n_tfr_writers))
 
     # extract subject IDs
     subjects = [int(f.split('sub-')[1]) for f in os.listdir(path) if 'sub' in f]
-    n_tfr_writers = 1
 
-    # HCP exp descriptors
-    TR = 0.72
+    # HCP exp. descriptors
+    TR = 0.72 # TR in seconds
     tasks = ['EMOTION',
              'GAMBLING',
              'LANGUAGE',
@@ -37,7 +42,7 @@ if __name__ == "__main__":
              'SOCIAL',
              'WM']
     runs = ['LR', 'RL']
-    n_classes_per_task = [2, 3, 2, 5, 2, 2, 4]
+    n_classes_per_task = [2, 3, 2, 5, 2, 2, 4] # number of decoding targets per task
 
     # tfr path
     tfr_path = path+'tfr/'
@@ -45,15 +50,22 @@ if __name__ == "__main__":
     
     # write data
     for subject_id, subject in enumerate(subjects):
-    	for task_id, task in enumerate(tasks):
-    		for run_id, run in enumerate(runs):
-    			tfr_writers = [tf.python_io.TFRecordWriter(
-    				tfr_path+'task-{}_subject-{}_{}.tfrecords'.format(task, subject, wi)) for wi in range(n_tfr_writers)]
-	    		subject_data = tfMRI_HCP_downloader.data.load_subject_data(task, subject, run, path, TR)
-	    		volumes, volume_labels = tfMRI_HCP_downloader.preprocess.preprocess_subject_data(
-	    			subject_data, [run], high_pass=1./128., smoothing_fwhm=3)
-	    		tfMRI_HCP_downloader.convert.write_to_tfr(tfr_writers,
-										                  volumes, volume_labels,
-										                  subject_id, task_id, run_id, n_classes_per_task,
-										                  randomize_volumes=Tru)
-	    
+        print('Processing subject: {}/{}'.format(subject_id+1, len(subjects)))
+        for task_id, task in enumerate(tasks):
+            for run_id, run in enumerate(runs):
+                # create TFR-writers
+                tfr_writers = [tf.python_io.TFRecordWriter(
+                    tfr_path+'task-{}_subject-{}_run-{}_{}.tfrecords'.format(task, subject, run, wi))
+                    for wi in range(n_tfr_writers)]
+                # load subject data
+                subject_data = tfMRI_HCP_downloader.data.load_subject_data(task, subject, run, path, TR)
+                # preprocess subject data
+                volumes, volume_labels = tfMRI_HCP_downloader.preprocess.preprocess_subject_data(
+                    subject_data, [run], high_pass=1./128., smoothing_fwhm=3)
+                # write preprocessed data to TFR
+                tfMRI_HCP_downloader.convert.write_to_tfr(tfr_writers,
+                                                          volumes.get_data(), volume_labels,
+                                                          subject_id, task_id, run_id, n_classes_per_task,
+                                                          randomize_volumes=True)
+                # close writers
+                [w.close() for w in tfr_writers]
