b30a2c9528b6b757be5dfdd842d84fa74c73c05a
rminthms <armin.thms@gmail.com>
2020-01-22 13:03:03 +0100

    adding docstring

--- hcprep/preprocess/preprocess.py
+++ hcprep/preprocess/preprocess.py
@@ -22,10 +22,32 @@ def _clean_func(func, mask, smoothing_fwhm=3, high_pass=1./128., tr=0.72):
     return unmasked_func
 
 
-def preprocess_subject_data(subject_data, runs, high_pass, smoothing_fwhm):
+def preprocess_subject_data(subject_data, runs, high_pass=None, smoothing_fwhm=None):
+    """Clean and return the voxel time-series signal of 
+    a subject in a task.
+
+    Clearning is performed with nilearn.signal.clean.
+
+    Args:
+        subject_data: Summary dict of the subject task-fMRI
+            data; created with hcprep.data.load_subject_data
+        runs: Runs for which to clean and return data
+            (one of ["LR", "RL"])
+        high_pass: High frequency cutoff in Hertz.
+        smoothing_fwhm: Smoothing strength, as a Full-Width 
+            at Half Maximum, in millimeters. If scalar is 
+            given, width is identical on all three directions.
+            A numpy.ndarray must have 3 elements, giving the
+            FWHM along each axis. If smoothing_fwhm is None, 
+            no filtering is performed.
+
+    Returns:
+        func: ndarray of cleaned voxel time-series signals
+        labels: labels for each TR.
+    """
 
     func = []
-    volume_label = []
+    labels = []
     for run in runs:
         # load tfMRI data
         func_run = load_img(subject_data[run]['func_mni'])
@@ -41,9 +63,9 @@ def preprocess_subject_data(subject_data, runs, high_pass, smoothing_fwhm):
         valid_trial_type = trial_type[valid_volume_idx]
 
         func.append(valid_func)
-        volume_label.append(valid_trial_type)
+        labels.append(valid_trial_type)
 
-    volume_label = np.concatenate(volume_label)
+    labels = np.concatenate(labels)
     func = concat_imgs(func)
 
-    return func, volume_label
+    return func, labels
